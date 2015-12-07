18c897275c02d7ec6e8dc3bef3f1896857227c2b
Fi1osof <info@newpg.ru>
2015-12-07 19:12:33 +0400

    1.4.0

--- _build/resolvers/resolve.install.php
+++ _build/resolvers/resolve.install.php
@@ -13,152 +13,156 @@ if ($object->xpdo) {
                     return $modx->log(modX::LOG_LEVEL_ERROR, "Could not load modTransportPackage class");
                 }
                 
-                /*
-                 * Packages need to install
-                 */
-                $packages = array(
-                    'phpTemplates',
-                    'modxSmarty',
-                    'Console',
-                    'Ace',
-                );
-                
-                $modx->log(modX::LOG_LEVEL_INFO, "Trying to install packages: ". implode(", ", $packages). "<br />\n
-                    Be patient. it`s take a several moments.");
-                
                 $modx->setLogLevel(modX::LOG_LEVEL_INFO);
                 $LogTarget = $modx->getLogTarget();  
                 $LogTopic = $LogTarget->subscriptions[0];
                 $messages = array();
+                
+                /*
+                 * Packages need to install
+                 */
+                $packages = array(
+                    # 'phpTemplates',
+                    # 'modxSmarty',
+                    # 'Console',
+                    # 'Ace',
+                );
+                
+                if($packages){
+                    
+                    $modx->log(modX::LOG_LEVEL_INFO, "Trying to install packages: ". implode(", ", $packages). "<br />\n
+                        Be patient. it`s take a several moments.");
+                        
+                    foreach($packages as $packageName){
+                        if(!$packageName)  continue;
+                        
+                        if($packageExists =  $modx->getObject('modTransportPackage', array(
+                            'package_name' => $packageName,
+                        ))){
+                            $messages[] = array(
+                                'level' => modX::LOG_LEVEL_INFO,
+                                'msg'   => "Package '{$packageName}' allready exists" ,
+                            );
+                                
+                            /*
+                             * Check is installed
+                             */
+                            if(!$packageExists->get('installed')){
+                                $messages[] = array(
+                                    'level' => modX::LOG_LEVEL_ERROR,
+                                    'msg'   => "Package '{$packageName}' exists, but not installed. Please, install it manually" ,
+                                );
+                            }
+                            continue;
+                        }
+                        // $modx->log(modX::LOG_LEVEL_INFO, "Try to get '{$packageName}' package.");
+                        
+                        $response = $modx->runProcessor('workspace/packages/rest/getlist', array(
+                            'provider' => 1,
+                            'query'  => $packageName,
+                        ));
+                        
+                        if($response->isError()){
+                            $messages[] = array(
+                                'level' => modX::LOG_LEVEL_ERROR,
+                                'msg'   => $response->getMessage(),
+                            );
+                            continue;
+                        }
+                        
+                        if(!$result = json_decode($response->getResponse())){ 
+                            $messages[] = array(
+                                'level' => modX::LOG_LEVEL_ERROR,
+                                'msg'   => "Error while reading results" ,
+                            );
+                            continue;
+                        }
+                        
+                        /*
+                         * Try to find package info
+                         */
+                        foreach($result->results as $r){
+                            $package = (array)$r;
+                            if($package['name'] == $packageName){
+                                goto downloadPackage;
+                            }
+                        }
+                        
+                        /*
+                         * Info was not found
+                         */
+                        $messages[] = array(
+                            'level' => modX::LOG_LEVEL_ERROR,
+                            'msg'   => "Package '{$packageName}' was not found. Please try to install it manually" ,
+                        );
+                        continue;
+                        
+                        
+                        /*
+                         * Download Package
+                         */
+                        downloadPackage: 
+                        $messages[] = array(
+                                'level' => modX::LOG_LEVEL_INFO,
+                                'msg'   => "Trying to download '{$packageName}' package" ,
+                        );
+                                
+                        $response = $modx->runProcessor('workspace/packages/rest/download', array(
+                            'provider' => 1,
+                            'info'  =>  "{$package['location']}::{$package['signature']}",
+                        ));
+                        
+                        if($response->isError()){
+                            $messages[] = array(
+                                'level' => modX::LOG_LEVEL_ERROR,
+                                'msg'   => "Failed download '{$packageName}'. ".$response->getMessage(),
+                            );
+                            continue;
+                        }
+                        $result = $response->getResponse();
+                        if($result['success'] == 1){
+                            $messages[] = array(
+                                'level' => modX::LOG_LEVEL_INFO,
+                                'msg'   => "Package '{$packageName}' downloaded success" ,
+                            );
+                            goto installPackage;
+                        }
+                        $messages[] = array(
+                            'level' => modX::LOG_LEVEL_ERROR,
+                            'msg'   => "Failed download '{$packageName}'.",
+                        );
+                        
+                        
+                        /*
+                         * Installing Package
+                         */
+                        installPackage:
+                        $messages[] = array(
+                                'level' => modX::LOG_LEVEL_INFO,
+                                'msg'   => "Trying to install '{$packageName}' package" ,
+                        );
+                        
+                                
+                        $response = $modx->runProcessor('workspace/packages/install', array(
+                            'signature' => $package['signature'], 
+                        ));
+                        
+                        if($response->isError()){
+                            $messages[] = array(
+                                'level' => modX::LOG_LEVEL_ERROR,
+                                'msg'   => "Failed install '{$packageName}'. ". $response->getMessage(),
+                            );
+                            continue;
+                        }
+                        $messages[] = array(
+                            'level' => modX::LOG_LEVEL_INFO,
+                            'msg'   => $response->getMessage() ,
+                        );
+                    }
+                }
 
                 
                  
-                foreach($packages as $packageName){
-                    if(!$packageName)  continue;
-                    
-                    if($packageExists =  $modx->getObject('modTransportPackage', array(
-                        'package_name' => $packageName,
-                    ))){
-                        $messages[] = array(
-                            'level' => modX::LOG_LEVEL_INFO,
-                            'msg'   => "Package '{$packageName}' allready exists" ,
-                        );
-                            
-                        /*
-                         * Check is installed
-                         */
-                        if(!$packageExists->get('installed')){
-                            $messages[] = array(
-                                'level' => modX::LOG_LEVEL_ERROR,
-                                'msg'   => "Package '{$packageName}' exists, but not installed. Please, install it manually" ,
-                            );
-                        }
-                        continue;
-                    }
-                    // $modx->log(modX::LOG_LEVEL_INFO, "Try to get '{$packageName}' package.");
-                    
-                    $response = $modx->runProcessor('workspace/packages/rest/getlist', array(
-                        'provider' => 1,
-                        'query'  => $packageName,
-                    ));
-                    
-                    if($response->isError()){
-                        $messages[] = array(
-                            'level' => modX::LOG_LEVEL_ERROR,
-                            'msg'   => $response->getMessage(),
-                        );
-                        continue;
-                    }
-                    
-                    if(!$result = json_decode($response->getResponse())){ 
-                        $messages[] = array(
-                            'level' => modX::LOG_LEVEL_ERROR,
-                            'msg'   => "Error while reading results" ,
-                        );
-                        continue;
-                    }
-                    
-                    /*
-                     * Try to find package info
-                     */
-                    foreach($result->results as $r){
-                        $package = (array)$r;
-                        if($package['name'] == $packageName){
-                            goto downloadPackage;
-                        }
-                    }
-                    
-                    /*
-                     * Info was not found
-                     */
-                    $messages[] = array(
-                        'level' => modX::LOG_LEVEL_ERROR,
-                        'msg'   => "Package '{$packageName}' was not found. Please try to install it manually" ,
-                    );
-                    continue;
-                    
-                    
-                    /*
-                     * Download Package
-                     */
-                    downloadPackage: 
-                    $messages[] = array(
-                            'level' => modX::LOG_LEVEL_INFO,
-                            'msg'   => "Trying to download '{$packageName}' package" ,
-                    );
-                            
-                    $response = $modx->runProcessor('workspace/packages/rest/download', array(
-                        'provider' => 1,
-                        'info'  =>  "{$package['location']}::{$package['signature']}",
-                    ));
-                    
-                    if($response->isError()){
-                        $messages[] = array(
-                            'level' => modX::LOG_LEVEL_ERROR,
-                            'msg'   => "Failed download '{$packageName}'. ".$response->getMessage(),
-                        );
-                        continue;
-                    }
-                    $result = $response->getResponse();
-                    if($result['success'] == 1){
-                        $messages[] = array(
-                            'level' => modX::LOG_LEVEL_INFO,
-                            'msg'   => "Package '{$packageName}' downloaded success" ,
-                        );
-                        goto installPackage;
-                    }
-                    $messages[] = array(
-                        'level' => modX::LOG_LEVEL_ERROR,
-                        'msg'   => "Failed download '{$packageName}'.",
-                    );
-                    
-                    
-                    /*
-                     * Installing Package
-                     */
-                    installPackage:
-                    $messages[] = array(
-                            'level' => modX::LOG_LEVEL_INFO,
-                            'msg'   => "Trying to install '{$packageName}' package" ,
-                    );
-                    
-                            
-                    $response = $modx->runProcessor('workspace/packages/install', array(
-                        'signature' => $package['signature'], 
-                    ));
-                    
-                    if($response->isError()){
-                        $messages[] = array(
-                            'level' => modX::LOG_LEVEL_ERROR,
-                            'msg'   => "Failed install '{$packageName}'. ". $response->getMessage(),
-                        );
-                        continue;
-                    }
-                    $messages[] = array(
-                        'level' => modX::LOG_LEVEL_INFO,
-                        'msg'   => $response->getMessage() ,
-                    );
-                }
                 
                 
                 // Update modxSmarty template path
