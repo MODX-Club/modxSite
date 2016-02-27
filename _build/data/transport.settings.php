ac6b792a997f82f8a528e1d5c1c20633ca062aa0
root <root@dev3.modx.site>
2016-02-27 19:38:48 +0300

    1.6.0-beta

--- _build/data/transport.settings.php
+++ _build/data/transport.settings.php
@@ -12,6 +12,26 @@ $setting->fromArray(array(
 ),'',true,true);
 $settings[] = $setting;
 
+$setting = $modx->newObject('modSystemSetting');
+$setting->fromArray(array(
+    'key' => 'modxsite.thumb_snippet',     // set unique key
+    'value' => '',
+    'xtype' => 'textfield',     //  textfield, numberfield, combo-boolean or other
+    'namespace' => NAMESPACE_NAME,
+    'area' => 'site',
+),'',true,true);
+$settings[] = $setting;
+
+$setting = $modx->newObject('modSystemSetting');
+$setting->fromArray(array(
+    'key' => 'modxsite.thumb_types',     // set unique key
+    'value' => '{"small":"&q=90&f=jpg&w=280&h=210","medium":"&q=90&f=jpg&w=600&h=375","big":"&q=90&f=jpg&w=1000&h=750"}',
+    'xtype' => 'textarea',     //  textfield, numberfield, combo-boolean or other
+    'namespace' => NAMESPACE_NAME,
+    'area' => 'site',
+),'',true,true);
+$settings[] = $setting;
+
 
 unset($setting);
 return $settings;
